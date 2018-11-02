6230c5fe31bd904498ad6096a7dbf32d1d2b17ad
cooks2093 <charlescooks@gmail.com>
2018-11-02 19:25:46 +0100

    charles commit

--- share.py
+++ share.py
@@ -1,1 +1,8 @@
 print('Hello guys')
+print ('hello Jo, this is Charles')
+def drinkbeer(x):
+    for i in (x):
+        print ('I drink  '  +i+ '   beer')
+
+beers = ['hoegaarden', 'duvel', 'orval']
+drinkbeer(beers)
